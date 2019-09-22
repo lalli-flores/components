cf9af0c21be6abf6217083c0acbcafd0feb9a1e6
lalli-flores <lalli.flores@gmail.com>
2019-09-22 16:34:09 -0700

    feat(RoomsAdapter): add hasMoreActivities to interface
    Additionally, getPreviousRoomActivities returns an array instead of an observable because previous data needs to be pulled instead pushed

--- src/adapters/RoomsAdapter.test.js
+++ src/adapters/RoomsAdapter.test.js
@@ -11,25 +11,11 @@ describe('Rooms Adapter Interface', () => {
     expect(rxjs.isObservable(roomsAdapter.getRoom())).toBeTruthy();
   });
 
-  test('getRoom() throws a proper error message', (done) => {
-    roomsAdapter.getRoom('id').subscribe(
+  test('getRoom() throws error since it is not defined', (done) => {
+    roomsAdapter.getRoom('ID').subscribe(
       () => {},
       (error) => {
         expect(error.message).toBe('getRoom(ID) must be defined in RoomsAdapter');
-        done();
-      }
-    );
-  });
-
-  test('getPreviousRoomActivities() returns an observable', () => {
-    expect(rxjs.isObservable(roomsAdapter.getPreviousRoomActivities())).toBeTruthy();
-  });
-
-  test('getPreviousRoomActivities() throws a proper error message', (done) => {
-    roomsAdapter.getPreviousRoomActivities('id').subscribe(
-      () => {},
-      (error) => {
-        expect(error.message).toBe('getPreviousRoomActivities(ID) must be defined in RoomsAdapter');
         done();
       }
     );
@@ -39,7 +25,7 @@ describe('Rooms Adapter Interface', () => {
     expect(rxjs.isObservable(roomsAdapter.getRoomActivities())).toBeTruthy();
   });
 
-  test('getRoomActivities() throws a proper error message', (done) => {
+  test('getRoomActivities() throws error since it is not defined', (done) => {
     roomsAdapter.getRoomActivities('id').subscribe(
       () => {},
       (error) => {
@@ -47,6 +33,21 @@ describe('Rooms Adapter Interface', () => {
         done();
       }
     );
+  });
+
+  test('getPreviousRoomActivities() returns a promise rejection since it is not defined', async () => {
+    try {
+      await roomsAdapter.getPreviousRoomActivities('ID');
+    } catch (error) {
+      expect(error.message).toEqual('getPreviousRoomActivities(ID) must be defined in RoomsAdapter');
+    }
+  });
+
+  test('hasMoreActivities() throws error since it is not defined', () => {
+    try {
+      expect(roomsAdapter.hasMoreActivities('ID')).toThrowError();
+      // eslint-disable-next-line no-empty
+    } catch (error) {}
   });
 
   afterEach(() => {
